cbe0b98f2b029893d918608765082669ef5c2d67
Yotaro Yamaguchi <yyamaguchi643@gmail.com>
2022-08-24 22:35:54 +0900

    Add tmux config

---
+++ tmux/install.sh
@@ -0,0 +1,9 @@
+# tmux
+# echo "============ Install tmux ============ "
+# TODO 
+
+# tpm
+# https://github.com/tmux-plugins/tpm
+echo "============ Install tpm ============ "
+git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+
